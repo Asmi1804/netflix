33136d4995514b494cafdbcf3e9066a9c642c7e8
Asmi1804 <162876660+Asmi1804@users.noreply.github.com>
2024-10-12 17:42:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+// Scroll behavior to smooth transition
+document.querySelector('.email-form button').addEventListener('click', function () {
+    document.getElementById('trendingSection').scrollIntoView({ behavior: 'smooth' });
+});
